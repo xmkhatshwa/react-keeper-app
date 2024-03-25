7570af6d0e3ac3dd34e50f351d0f934340a6efc5
xmkhatshwa <mkhatshwa.xola@gmail.com>
2024-03-25 13:06:23 +0200

    Add TodoTable Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body"></div>
+      </div>
     </div>
   );
 }
